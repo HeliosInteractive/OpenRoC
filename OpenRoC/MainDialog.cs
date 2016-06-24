74cd0a7fcf7b21691884e1f54fb627d96bfc0c05
Sepehr Laal <sepehr.laal@gmail.com>
2016-06-24 10:42:51 -0700

    Checked in the base Visual Studio WinForms project

---
+++ OpenRoC/MainDialog.cs
@@ -0,0 +1,12 @@
+﻿namespace oroc
+{
+    using System.Windows.Forms;
+
+    public partial class MainDialog : Form
+    {
+        public MainDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OpenRoC/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace oroc
+{
+    using System;
+    using System.Windows.Forms;
+
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainDialog());
+        }
+    }
+}
