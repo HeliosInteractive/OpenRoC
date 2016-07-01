1c0bfc28a9fc090863512000d0ab93fc75d3cb73
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-01 13:05:05 -0700

    Added status texts to Settings dialog

--- OpenRoC/SettingsDialog.cs
+++ OpenRoC/SettingsDialog.cs
@@ -8,6 +8,22 @@
         {
             InitializeComponent();
             SetupDataBindings();
+
+            HandleCreated += OnSettingsDialogHandleCreated;
+        }
+
+        private void OnSettingsDialogHandleCreated(object sender, System.EventArgs e)
+        {
+            if (!(Owner is MainDialog))
+            {
+                Log.e("Owner of Settings Window has changed but it is not the right type.");
+                return;
+            }
+
+            MainDialog main_dialog = Owner as MainDialog;
+
+            main_dialog.SetStatusBarText(SingleInstanceCheckBox, "Allow only one instance of OpenRoC to run on this computer.");
+            main_dialog.SetStatusBarText(StartMinimizedCheckBox, "Start OpenRoC minimized, in task-bar next time it launches.");
         }
 
         private void SetupDataBindings()
