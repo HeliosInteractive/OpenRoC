8f7d0774978776236d4e47e09095717418c10c8f
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-04 21:40:57 -0700

    added an unresponsive mode to test gui process

--- OpenRoC/testProcessWindowed/Form1.cs
+++ OpenRoC/testProcessWindowed/Form1.cs
@@ -1,20 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace testProcessWindowed
+﻿namespace testProcessWindowed
 {
+    using System;
+    using System.Threading;
+    using System.Windows.Forms;
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+            Shown += OnShown;
+        }
+
+        private void OnShown(object sender, EventArgs e)
+        {
+            string[] args = Environment.GetCommandLineArgs();
+
+            if (args != null && args.Length > 1)
+            {
+                bool unresponsive;
+                if (bool.TryParse(args[1], out unresponsive) && unresponsive)
+                    Thread.Sleep(TimeSpan.FromMinutes(30));
+            }
         }
     }
 }
