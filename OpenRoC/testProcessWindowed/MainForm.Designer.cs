c94f2ca81e6844e7d7fa70b77666566b96f64763
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-27 14:41:46 -0700

    renamed Form1 in test process project and added an option to perform timed unresponsiveness

--- OpenRoC/testProcessWindowed/MainForm.Designer.cs
+++ OpenRoC/testProcessWindowed/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace testProcessWindowed
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.SuspendLayout();
             // 
             // Form1
