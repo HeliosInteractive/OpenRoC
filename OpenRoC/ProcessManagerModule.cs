580a539362693f9a3c3815dda5d9311cb576f261
Sepehr Laal <sepehr.laal@gmail.com>
2016-07-28 13:54:07 -0700

    added a page for every process name

--- OpenRoC/ProcessManagerModule.cs
+++ OpenRoC/ProcessManagerModule.cs
@@ -1,7 +1,11 @@
 ﻿namespace oroc
 {
     using Nancy;
+
     using liboroc;
+
+    using System.IO;
+    using System.Linq;
 
     public class ProcessManagerModule : NancyModule
     {
@@ -16,12 +20,35 @@
         private void SetupRoutes()
         {
             Get["/"] = IndexPage;
-            Get["/{uri*}"] = IndexPage;
+            Get["/{process}"] = ProcessPage;
         }
 
         private Response IndexPage(dynamic input)
         {
-            return processManager.ProcessRunnerList.ToJsonResponse();
+            return processManager.ProcessRunnerList
+                .Select(proc => new
+                {
+                    name = Path.GetFileName(proc.ProcessOptions.Path),
+                    time = proc.Stopwatch.ElapsedMilliseconds,
+                    stat = proc.State
+                })
+                .ToArray()
+                .ToJsonResponse();
+        }
+
+        private Response ProcessPage(dynamic input)
+        {
+            string process = input["process"];
+
+            return processManager.ProcessRunnerList
+                .Where(proc => Path.GetFileName(proc.ProcessOptions.Path) == process)
+                .Select(proc => new
+                {
+                    time = proc.Stopwatch.ElapsedMilliseconds,
+                    stat = proc.State
+                })
+                .ToArray()
+                .ToJsonResponse();
         }
     }
 }
