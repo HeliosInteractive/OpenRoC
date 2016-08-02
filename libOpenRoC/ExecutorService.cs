529705c2fbaa78d860f3c1ba0295b272d0d88952
Sepehr Laal <sepehr.laal@gmail.com>
2016-08-02 11:36:19 -0700

    Added an executor service to queue stuff to be executed in the background

---
+++ libOpenRoC/ExecutorService.cs
@@ -0,0 +1,80 @@
+﻿namespace liboroc
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    public class ExecutorService : IDisposable
+    {
+        private readonly List<Task> pending;
+        public Action<Exception> ExceptionReceived;
+
+        public ExecutorService()
+        {
+            pending = new List<Task>();
+        }
+
+        public void Accept(Action action)
+        {
+            if (IsDisposed)
+                return;
+
+            lock (this)
+            {
+                pending?.Add(Task.Run(() =>
+                {
+                    try { action(); }
+                    catch (Exception ex)
+                    { ExceptionReceived?.Invoke(ex); }
+                }));
+            }
+        }
+
+        public void Wait()
+        {
+            if (IsDisposed)
+                return;
+
+            lock (this)
+            {
+                try
+                {
+                    Task.WaitAll(pending.ToArray());
+                }
+                catch (ObjectDisposedException ex)
+                {
+                    ExceptionReceived?.Invoke(ex);
+                }
+                catch (AggregateException ex)
+                {
+                    ExceptionReceived?.Invoke(ex);
+                }
+
+                pending.ForEach(task => task.Dispose());
+                pending.Clear();
+            }
+        }
+
+        #region IDisposable Support
+        public bool IsDisposed { get; private set; } = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!IsDisposed)
+            {
+                if (disposing)
+                {
+                    Wait();
+                }
+
+                IsDisposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+    }
+}
---
+++ testOpenRoC/ExecutorServiceUnitTests.cs
@@ -0,0 +1,74 @@
+﻿namespace testOpenRoC
+{
+    using liboroc;
+
+    using System;
+    using System.Threading;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ExecutorServiceUnitTests
+    {
+        [TestMethod]
+        public void WaitForPending()
+        {
+            bool flag1 = false;
+            bool flag2 = false;
+
+            using (ExecutorService service = new ExecutorService())
+            {
+                service.Accept(() => { Thread.Sleep(TimeSpan.FromSeconds(1)); flag1 = true; });
+                service.Accept(() => { Thread.Sleep(TimeSpan.FromSeconds(1)); flag2 = true; });
+            }
+
+            Assert.IsTrue(flag1);
+            Assert.IsTrue(flag2);
+        }
+
+        [TestMethod]
+        public void WaitAfterException()
+        {
+            bool flag1 = false;
+            bool flag2 = false;
+
+            using (ExecutorService service = new ExecutorService())
+            {
+                service.Accept(() =>
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    throw new Exception();
+
+                });
+                service.Accept(() => { Thread.Sleep(TimeSpan.FromSeconds(1)); flag2 = true; });
+            }
+
+            Assert.IsFalse(flag1);
+            Assert.IsTrue(flag2);
+        }
+
+        [TestMethod]
+        public void ExceptionPropagation()
+        {
+            Exception ex1 = new Exception();
+            Exception ex2 = null;
+
+            using (ExecutorService service = new ExecutorService())
+            {
+                service.ExceptionReceived += (ex) =>
+                {
+                    ex2 = ex;
+                };
+
+                service.Accept(() =>
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(1));
+                    throw ex1;
+
+                });
+            }
+
+            Assert.IsTrue(ReferenceEquals(ex1, ex2));
+        }
+    }
+}
